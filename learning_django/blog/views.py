de0934a28f996aa53cc5ccf51880f33733e597c7
metasignal <timcollinsdesign+metasignal@gmail.com>
2023-01-05 23:00:56 -0500

    added about page for blog

--- learning_django/blog/views.py
+++ learning_django/blog/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 def home(request):
   return HttpResponse('<h1>Blog home</h1>')
 
+def about(request):
+  return HttpResponse('<h1>Blog About</h1>')
--- learning_django/learning_django/urls.py
+++ learning_django/learning_django/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', includes('blog.urls')),
+    path('blog/', include('blog.urls')),
 ]
