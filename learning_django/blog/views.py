88817b7270ec7e83edb3427a11ab1f98bbe5457c
metasignal <timcollinsdesign+metasignal@gmail.com>
2023-01-06 23:35:23 -0500

    templates set up

--- learning_django/blog/views.py
+++ learning_django/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Blog home</h1>')
+  return render(request, 'blog/home.html')
 
 def about(request):
-  return HttpResponse('<h1>Blog About</h1>')
+  return render(request, 'blog/about.html')
