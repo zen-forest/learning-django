6a77d74f53a5a0552991be3aaece3538f9968b69
metasignal <timcollinsdesign+metasignal@gmail.com>
2023-01-07 12:24:31 -0500

    templating with mock data working

--- learning_django/blog/views.py
+++ learning_django/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+  {
+    'author': 'Tim Collins',
+    'title': 'Blog post',
+    'content' : 'First post content',
+    'date_posted': 'January 7th, 2023'
+  },
+  {
+    'author': 'Dieter Rams',
+    'title': '10 Principles of design',
+    'content' : 'Dieter was here',
+    'date_posted': 'January 7th, 2023'
+  }
+]
 
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
   return render(request, 'blog/about.html')
